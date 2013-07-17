617cb01be478f4f3dff117a7d7f372a4b8ec420a
Susana Ini <susan_susan_susan1234@yahoo.es>
2013-07-17 16:00:58 +0000

    Modified web.js for reading index.html

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
-var fs = require('fs');
-var buffer = new Buffer();
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello");
+  response.send('ello');
 });
 
 var port = process.env.PORT || 5000;
